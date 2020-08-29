d67d4a7a62c5ce28ef75144177fc90c9af85cc98
Alexsandro Valencio <alexsandrova@hotmail.com>
2020-08-29 17:47:48 -0300

    add AppProvider

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,11 @@ import AuthRoutes from './routes';
 const App: React.FC = () => (
   <NavigationContainer >
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <AppProvider>
       <View style={{ flex: 1, backgroundColor: '#312e38' }}>
         <AuthRoutes />
       </View>
+    </AppProvider>
   </NavigationContainer>
 );
 
